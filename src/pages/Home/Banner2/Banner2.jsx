5005f284af0a81dc591cab18e9492f9869f82ef5
csesopnil <sopnilstar@gmail.com>
2023-11-21 18:21:28 +0600

    picture added: g:

--- src/pages/Home/Banner2/Banner2.jsx
+++ src/pages/Home/Banner2/Banner2.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import bannerimg from '/home/chef-service.jpg'
+
 const Banner2 = () => {
     return (
         <div >
-            <div className="  bg-[url('./home/chef-service.jpg')]  my-5 md:bg-cover md:bg-no-repeat bg-center  ">
+            <div className= {`bg-[url('${bannerimg}')]  my-5 md:bg-cover md:bg-no-repeat bg-center  `}>
             <div className=' w-10/12 md:w-10/12 mx-auto md:h-[450px] flex justify-center items-center '>
                     <div className='bg-white md:h-[250px] rounded-lg  flex justify-center  items-center' >
                     <div className='space-y-4  '>
